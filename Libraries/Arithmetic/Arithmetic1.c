7eb1d9e78465980fe5115f849c65a6d5c21fa72a
DineshKadam97 <132736142+DineshKadam97@users.noreply.github.com>
2023-07-21 22:23:31 +0530

    Arithmetic1.c

---
+++ Libraries/Arithmetic/Arithmetic1.c
@@ -0,0 +1,15 @@
+#include"Arithmetic.h"
+
+int Addition(int iNo1, int iNo2)
+{
+    int iAns = 0;
+    iAns = iNo1 + iNo2;
+    return iAns;
+}
+
+int Substraction(int iNo1, int iNo2)
+{
+    int iAns = 0;
+    iAns = iNo1 - iNo2;
+    return iAns;
+}
