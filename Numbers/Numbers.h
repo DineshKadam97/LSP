cb49fb9fd80f5c54840a9afdaf3cd4c86789a0eb
DineshKadam97 <132736142+DineshKadam97@users.noreply.github.com>
2023-07-21 21:56:24 +0530

    Update Numbers.h

--- Numbers/Numbers.h
+++ Numbers/Numbers.h
@@ -1,11 +1,11 @@
 bool CheckEven(int);
 bool CheckOdd(int);
-bool CheckPerfect(int );
-bool CheckPrime(int );
+bool CheckPerfect(int);
+bool CheckPrime(int);
 
-void DisplayFactors(int );
-int FactorsSum(int );
-float FactorsAvg(int );
+void DisplayFactors(int);
+int FactorsSum(int);
+float FactorsAvg(int);
 
 unsigned long int Factorial(int);
 unsigned long int Exponent(int , int);
