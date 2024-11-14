f12443f8dc7c5a717f3fb2a66965b2c540dccd3e
DineshKadam97 <132736142+DineshKadam97@users.noreply.github.com>
2024-11-14 14:14:01 +0000

    Add files via upload

---
+++ Multiprocessing/CountCapital.c
@@ -0,0 +1,34 @@
+#include<stdio.h>
+#include<fcntl.h>
+#include<string.h>
+#include<unistd.h>
+
+int main()
+{
+  int fd = 0, iRet = 0;
+  int iCount = 0;
+  char Buffer[1024];
+  
+  fd = open("Demo.txt",O_RDONLY);
+  if(fd == -1)
+  {
+    perror("Can not open file\n");
+    return -1;
+  }
+  
+  while((iRet = read(fd,Buffer,sizeof(Buffer))) != 0)
+  {
+    for(int i = 0; i < iRet; i++)
+    {
+      if((Buffer[i] >= 'A') && (Buffer[i] <= 'Z'))
+      {
+        iCount++;
+      }
+    }
+    memset(Buffer,0,sizeof(Buffer));
+  }
+  
+  printf("Number of capital letters are : %d\n",iCount);
+  close(fd);
+  return 0;
+}
